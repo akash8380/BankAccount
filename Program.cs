88e7860086c3674911ab9a342330cbd06fadb205
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-11-08 20:26:37 +0530

    Present Or Not

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage COmputation");
+            int employeePresent = 1;
+            Random r = new Random();
+            int employeeInput = r.Next(0, 2);
+
+            if (employeePresent == employeeInput)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadLine();
+           
+        }
+    }
+}
